92bae18b4c75eb962414545a8b89ec8fdb7ad939
Viljar P?vvat <viljar.povvat@redfunction.ee>
2015-03-25 15:25:39 +0200

    added gauge celery task

--- url_metric/tasks.py
+++ url_metric/tasks.py
@@ -63,6 +63,29 @@ def metric(metric_name, value=1, logger_prefix = None):
 
     return result
 
+@task(name="url_metric.gauge")
+def gauge(metric_name, value=1, logger_prefix = None):
+    logger_extra = ''
+    if logger_prefix:
+        logger_extra = ".%s" % logger_prefix
+
+    result = None
+
+    try:
+        exporter = exports.get_exporter()
+        debug_logger = logging.getLogger('external.debug%s' % logger_extra)
+        if exporter:
+            result = exporter.gauge(metric_name, value)
+            debug_logger.debug("source: %s %s +%s" % (exporter.source, metric_name, value))
+        else:
+            debug_logger.debug("exporter: %s" % exporter)
+
+    except Exception, e:
+        error_logger = logging.getLogger('external.error%s' % logger_extra)
+        error_logger.exception(metric_name)
+
+    return result
+
 @task(name="url_metric.counter")
 def counter(metric_name, value=1, logger_prefix = None):
     logger_extra = ''
